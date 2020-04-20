80d790260bb7ed8300a2253917ddc61d46ba6e9e
yoobetter <bestdbskdms@naver.com>
2020-04-20 16:53:33 +0900

    fsdf

---
+++ indeed_study.py
@@ -0,0 +1,20 @@
+tuto  = "4/20"
+#for i in a # 여기에서 a에는 무조건 list가 들어가야 함
+# find() 하면 그 결과값은 리스트가 아닌 string
+# 따라서 for문에 넣으려면 list 값이 나오는 find_all을 해야 함
+'''
+2차원 list인 a가 있으면 
+for i in a:
+    print a
+를 하면 
+[x x x]
+[x x x]
+[x x x] 이렇게 출력됨 --> 엑셀 같은 곳에서 내가 원하는 형태 만들려면 2차원 리스트를 만들어야 하는 것
+'''
+'''
+git에 올리는 방법
+git init
+git remote add origin github주소
+git fetch origin
+git merge origin master
+'''
